0fc7f70de2532cfd8e514cc3723a871fa0177197
Shaik Mohd Huzaifa <shaikmohdhuz@gmail.com>
2024-11-21 10:23:26 +0530

    Prompt Up

--- src/Components/PromptContainer/PromptContainer.component.jsx
+++ src/Components/PromptContainer/PromptContainer.component.jsx
@@ -2,21 +2,27 @@ import { useEffect, useState } from "react";
 import { PromptSelector } from "../../store/prompt/prompt.selector.js";
 import "./PromptContainer.styles.scss"; // Correcting the style import
 import { useSelector } from "react-redux";
+import { useRef } from "react";
 
 export const PromptContainer = () => {
+  const promptContainerRef = useRef(null);
   const prompts = useSelector(PromptSelector);
-  const [localPrompts, setPrompts] = useState([]);
 
   useEffect(() => {
-    setPrompts(prompts);
+    // Scroll to the bottom of the container whenever userPrompts change
+    if (promptContainerRef.current) {
+      promptContainerRef.current.scrollTop =
+        promptContainerRef.current.scrollHeight;
+    }
   }, [prompts]);
-
   return (
     <div className="prompt-container">
-      {localPrompts && localPrompts.length > 0 ? (
-        localPrompts.map((prompt, index) => (
+      {prompts && prompts.length > 0 ? (
+        prompts.map((prompt, index) => (
           <div key={index} className={`prompt ${prompt.type}`}>
-            {prompt.prompt}
+            <p>
+              {prompt.type == "user" ? prompt.prompt : prompt.prompt.response}
+            </p>
           </div>
         ))
       ) : (
